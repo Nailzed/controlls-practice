c155158149a6736910048663af97825c77bc8817
Сергей Земсков <nailzed@yandex.ru>
2021-03-09 15:39:11 +0300

    created UI

--- controlls practice/ViewController.swift
+++ controlls practice/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var switches: [UISwitch]!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttonPressed() {
+    }
+    @IBAction func switchToggled(_ sender: UISwitch) {
+    }
+    @IBAction func sliderMoved() {
+    }
+    
+    @IBAction func textFieldEditted() {
+    }
+    
 }
 
